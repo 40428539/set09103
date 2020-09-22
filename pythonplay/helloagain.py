eeeb01e6e04495db0ac2911c4d878ec7fe41ee7f
Andrew Mclay <40428539@live.napier.ac.uk>
2020-09-22 10:49:43 +0100

    NEW addition

---
+++ pythonplay/helloagain.py
@@ -0,0 +1,18 @@
+print ("Hello again");
+
+h = ("Hello");
+a = ("Again");
+
+x = 0;
+
+while (x < 5):
+    print (" and " )
+    print (h + " " + a)
+
+    x += 1 ;
+        
+else: 
+    print ("Script successfull")
+
+       
+
