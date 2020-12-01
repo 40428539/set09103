73fcb01fbfae22fe46bce376bcc0514166db8448
40428539 <71348506+40428539@users.noreply.github.com>
2020-12-01 22:06:25 +0000

    Update schema.sql

--- htmlplay/schema.sql
+++ htmlplay/schema.sql
@@ -6,3 +6,14 @@ drop table if exists Users;
 	
 );
 
+drop table if exists Messages;
+	create table Messages (
+	ID integer primary key autoincrement,
+	Username text not null,
+	Message text,
+	Time timestamp,
+	Important boolean,
+	Roomname text not null
+	
+);
+
